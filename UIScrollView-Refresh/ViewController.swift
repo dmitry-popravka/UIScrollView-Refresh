edf2d29875094c65b8430b5b61e0a820b6a6d557
Dmitry Popravka <dmitry.popravka@gmail.com>
2018-02-15 20:25:49 +0200

    init

---
+++ UIScrollView-Refresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIScrollView-Refresh
+//
+//  Created by Dmitry on 15.02.18.
+//  Copyright © 2018 Dmitry Popravka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
